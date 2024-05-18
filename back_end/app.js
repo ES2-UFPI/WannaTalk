38c9e2da40d78dbb044f19f9b2d0cdd44688490f
Felipe Moura <felipemoura1407@gmail.com>
2024-05-18 12:48:39 -0300

    Configuração Inicial dos modulos

--- back_end/app.js
+++ back_end/app.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const app     = express()
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 
-app.get("/", (req,res) => {
-    res.json({nome: "teste"})
-})
+// Configurações
+
+    // Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
 
-const PORT = 3030
-app.listen(PORT, () => {
-    console.log("Conexão ativa na porta: "+ PORT)
-})
+
+    // Body Parser
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+
+
+// Rotas
+
+    app.get("/", (req,res) => {
+        res.json({nome: "teste"})
+    })
+
+
+
+
+// Conexão
+
+    const PORT = 3030
+    app.listen(PORT, () => {
+        console.log("Conexão ativa na porta: "+ PORT)
+    })
