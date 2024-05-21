395aad66e8de80dfc59403f5d6dabb25414acacf
Felipe Moura <felipemoura1407@gmail.com>
2024-05-21 09:32:30 -0300

    feat: Implementando rota de validação de usuário para acesso à página do chat

--- back_end/rotas/user.js
+++ back_end/rotas/user.js
@@ -1,9 +1,81 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/", (req,res) => {
-    res.send("Test")
-})
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+/* Prisma info
+findMany: Used to retrieve multiple records from a database table.
+findUnique: Used to retrieve a single record that matches a unique constraint.
+findFirst: Used to retrieve the first record that matches the given criteria.
+findRaw: Used for raw SQL queries.
+*/
+
+// User CREATE
+    router.get('/new', (req,res) => {
+        res.render("user/userform")
+    })
+
+    router.post("/new", async (req,res) => {
+        const newUser = await prisma.user.create({
+            data:{
+                name: req.body.name,
+                email: req.body.email,
+                hashcode: req.body.email + req.body.password
+            }
+        })
+        console.log("Registrado com sucesso")
+        res.redirect("/list")
+
+    })
+
+
+
+// USER LIST
+    router.get('/list', async (req,res) => {
+        const list = await prisma.user.findMany();
+        res.json(list)
+    })
+
+
+
+
+
+
+
+
+
+// Rotas
+
+    // Inicial
+
+    router.get("/", (req,res) => {
+        res.send("Test")
+    })
+
+
+
+// Conversa com chat
+    router.get("/:hashcode/chat", (req, res) => {
+        prisma.user.findFirst({
+            where: {
+            hashcode: req.params.hashcode
+        }}).then((user) => {
+            // Validação ok
+            res.json(user)
+//            res.render("/target") Renderizar a tela do chat
+
+        }).catch((err) => {
+            console.log(err)
+            res.redirect("/user/list")
+        })
+    })
+
+
+
+
+
+
 
 
 module.exports = router
