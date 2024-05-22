2745a83f955e2ac87ac59cde740a6e61bb760be6
enzolda <enzolealdearagao@gmail.com>
2024-05-22 17:16:01 -0300

    Adicionei a página principal

--- front_end/wannatalk/src/app/page.js
+++ front_end/wannatalk/src/app/page.js
@@ -2,111 +2,118 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="bg-white">
+        <div>
+          <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+              <div class="relative flex h-16 items-center justify-between">
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div class="flex flex-shrink-0 items-center">
+                    {/* <!-- Vai direcionar para tela inicial --> */}
+                    <a href="#" class="rounded-md text-xl text-white hover:bg-gray-700">WannaTalk?</a>
+                  </div>
+                  <div class="hidden sm:ml-6 sm:block">
+                    <div class="flex space-x-1">
+                      {/* <!-- Direciona para tela de visualizar roteiros/praticar roteiro--> */}
+                      <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Praticar Roteiro</a>
+                      <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Criar Roteiro</a>
+                      <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Pesquisar Roteiro</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+          <div class="bg flex">
+          <div class="flex flex-col">
+              <div class="m-3 rounded-md bg-red-700 p-2">
+                <div class="text-white">
+                  <div class="text-3xl">O que é o WannaTalk?</div>
+                  <div class="m-4 sm:mt-11">Nossa plataforma se propõe a ajudar você a treinar novas línguas em diversas situações</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-red-700 p-2">
+                <div class="text-white">
+                  <div class="text-3xl">Como vou aprender novas línguas?</div>
+                  <div class="m-4 sm:mt-11">Por meio de chats de Inteligência Artificial treinados por usuários da nossa plataforma para outros</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-red-700 p-2">
+                <div class="text-white">
+                  <div class="text-3xl">Lorem ipsum</div>
+                  <div class="m-4 sm:mt-11">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat dolor nec lectus porta mattis. Nulla facilisi. Duis sollicitudin mi.</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-red-700 p-2">
+                <div class="text-white">
+                  <div class="text-3xl">Lorem ipsum</div>
+                  <div class="m-4 sm:mt-11">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat dolor nec lectus porta mattis. Nulla facilisi. Duis sollicitudin mi.</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-red-700 p-2">
+                <div class="text-white">
+                  <div class="text-3xl">Lorem ipsum</div>
+                  <div class="m-4 sm:mt-11">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat dolor nec lectus porta mattis. Nulla facilisi. Duis sollicitudin mi.</div>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col">
+              <div class="m-3 rounded-md bg-green-700 p-2">
+                <div class="relative flex items-center text-white sm:items-stretch sm:justify-start">
+                  <div id="titulo">Título</div>
+                  <div id="autor" class="sm:ml-14">Feito por:</div>
+                  <div id="rank" class="sm:ml-1">| Rank</div>
+                  <div id="praticas" class="sm:ml-4">Práticas:</div>
+                  <div id="satisfacao" class="sm:ml-2">Satisfação: %</div>
+                </div>
+                <div class="relative flex items-center sm:justify-start">
+                  <button class="rounded-md bg-lime-400 p-1 hover:bg-lime-300">Acessar</button>
+                  <div class="text-white sm:ml-3">Resumo text-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-white</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-green-700 p-2">
+                <div class="relative flex items-center text-white sm:items-stretch sm:justify-start">
+                  <div id="titulo">Título</div>
+                  <div id="autor" class="sm:ml-14">Feito por:</div>
+                  <div id="rank" class="sm:ml-1">| Rank</div>
+                  <div id="praticas" class="sm:ml-4">Práticas:</div>
+                  <div id="satisfacao" class="sm:ml-2">Satisfação: %</div>
+                </div>
+                <div class="relative flex items-center sm:justify-start">
+                  <button class="rounded-md bg-lime-400 p-1 hover:bg-lime-300">Acessar</button>
+                  <div class="text-white sm:ml-3">Resumo text-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-white</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-green-700 p-2">
+                <div class="relative flex items-center text-white sm:items-stretch sm:justify-start">
+                  <div id="titulo">Título</div>
+                  <div id="autor" class="sm:ml-14">Feito por:</div>
+                  <div id="rank" class="sm:ml-1">| Rank</div>
+                  <div id="praticas" class="sm:ml-4">Práticas:</div>
+                  <div id="satisfacao" class="sm:ml-2">Satisfação: %</div>
+                </div>
+                <div class="relative flex items-center sm:justify-start">
+                  <button class="rounded-md bg-lime-400 p-1 hover:bg-lime-300">Acessar</button>
+                  <div class="text-white sm:ml-3">Resumo text-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-white</div>
+                </div>
+              </div>
+              <div class="m-3 rounded-md bg-green-700 p-2">
+                <div class="relative flex items-center text-white sm:items-stretch sm:justify-start">
+                  <div id="titulo">Título</div>
+                  <div id="autor" class="sm:ml-14">Feito por:</div>
+                  <div id="rank" class="sm:ml-1">| Rank</div>
+                  <div id="praticas" class="sm:ml-4">Práticas:</div>
+                  <div id="satisfacao" class="sm:ml-2">Satisfação: %</div>
+                </div>
+                <div class="relative flex items-center sm:justify-start">
+                  <button class="rounded-md bg-lime-400 p-1 hover:bg-lime-300">Acessar</button>
+                  <div class="text-white sm:ml-3">Resumo text-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-whitetext-white</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
