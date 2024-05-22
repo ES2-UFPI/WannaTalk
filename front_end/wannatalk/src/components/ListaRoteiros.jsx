b4481cadaaa0a846da4853701897a4f617161d8a
enzolda <enzolealdearagao@gmail.com>
2024-05-22 17:48:04 -0300

    Adicionei o jsx que vai listar os roteiros

--- front_end/wannatalk/src/components/ListaRoteiros.jsx
+++ front_end/wannatalk/src/components/ListaRoteiros.jsx
@@ -1,1 +1,22 @@
 import Roteiro from "./Roteiro";
+import roteiros from "../data/roteiros";
+
+function ListaRoteiros() {
+    return(
+        <div className="listaRoteiros">
+            {roteiros.length>0 ? (
+                roteiros.map(Roteiro => (
+                    <Roteiro
+                    key = {roteiros.id}
+                    roteiros = {roteiros} // Passando o objeto produto como uma propriedade chamada 'produto'
+                    />
+                ))
+            ): (
+                <div>Nenhum Roteiro disponível</div>
+            )}
+        </div>
+
+    );
+}
+
+export default ListaRoteiros; 
