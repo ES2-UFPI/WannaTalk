0370f99a7ab207f223287212762705d09eb71374
enzolda <enzolealdearagao@gmail.com>
2024-05-22 17:42:19 -0300

    Adicionei a estrutura de dados que vai ser simular o backend

---
+++ front_end/wannatalk/src/data/roteiros.js
@@ -0,0 +1,8 @@
+const roteiros = [
+    {id: 1, titulo: 'No aeroporto', autor: 'Enzo', rank: 'Novato', praticas: 23, satisfacao: 80},
+    {id: 2, titulo: 'No aeroporto', autor: 'Jordana', rank: 'Master', praticas: 50, satisfacao: 95},
+    {id: 3, titulo: 'No aeroporto', autor: 'Felipe', rank: 'Experiente', praticas: 27, satisfacao: 60},
+    {id: 4, titulo: 'No aeroporto', autor: 'Jhonn', rank: 'Novato', praticas: 40, satisfacao: 35}
+]
+
+export default roteiros;
