9a3d11b399fc2f3d4370c98ad858bd9f1e42b743
jordanabfranca <jordanafranca@ufpi.edu.br>
2024-05-25 21:11:15 -0300

    tela do chat com ia

--- front_end/wannatalk/src/app/pages/chatconversa/page.js
+++ front_end/wannatalk/src/app/pages/chatconversa/page.js
@@ -1,4 +1,6 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Chat } from "@/components/Chat";
+
+{/*import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -19,6 +21,9 @@ export default function Home(){
                         </Avatar>
                         <p className="leading-relaxed">
                             <span className="block font-bold text-slate-800">User:</span>
+                            Isso criará uma nova branch com o nome especificado. Lembre-se de substituir nome-da-sua-branch pelo nome que você deseja dar à sua nova branch.
+
+Se você também quiser mudar para essa nova branch imediatamente, você pode usar o seguinte comando:
                         </p>
                     </div>
 
@@ -38,6 +43,15 @@ export default function Home(){
                     <Button type = "submit"> Enviar </Button>
                 </CardFooter>
             </Card>
+        </div>
+    )
+}*/}
+
+export default function Home (){
+    console.log(Chat)
+    return(
+        <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+            <Chat/>
         </div>
     )
 }
---
+++ front_end/wannatalk/src/components/Chat.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Card,CardHeader,CardContent,CardTitle,CardDescription, CardFooter} from "./ui/card";
+import { Input } from "./ui/input";
+import { useChat } from "ai/react";
+import { ScrollArea } from "./ui/scroll-area";
+
+export function Chat(){
+    const { messages, input, handleInputChange, handleSubmit } = useChat(
+    )
+    return(
+        <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+            <Card className= "w-[800px] ">
+                <CardHeader>
+                    <CardTitle className="justify-center"> Titulo</CardTitle>
+                    <CardDescription> descricao </CardDescription>
+                </CardHeader>
+                
+                <CardContent >
+                    <ScrollArea className="w-full h-[400px]  pr-4">
+                        {messages.map(message => {
+                            return(
+                                <div key= {message.id}className="flex gap-3 text-slate-600 text-sm mb-4">
+                                    {message.role ==='user' && (
+                                        <Avatar>
+                                        <AvatarFallback>Us</AvatarFallback>
+                                        <AvatarImage src=""/>
+                                    </Avatar>
+                                    )}
+                                    {message.role ==='assistant' && (
+                                        <Avatar>
+                                        <AvatarFallback>CT</AvatarFallback>
+                                        <AvatarImage src=""/>
+                                    </Avatar>
+                                    )}
+                            
+                            <p className="leading-relaxed">
+                                <span className="block font-bold text-slate-800">
+                                    {message.role==='user'? 'Usuario': 'Chat'}:</span>
+                                    {message.content}
+                            </p>
+                        </div> 
+                            )
+                        })}
+                        </ScrollArea>
+                </CardContent>
+                
+                
+                <CardFooter >
+                    <form className= " space-x-2 w-full flex gap-2" onSubmit={handleSubmit}>
+                    <Input placeholder="Mensagem" value ={input} onChange ={handleInputChange}/>
+                    <Button type = "submit"> Enviar </Button>
+                    </form>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
