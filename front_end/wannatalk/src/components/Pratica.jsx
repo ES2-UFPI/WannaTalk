90d229fe2e9fdbf3f38dec95391ff1def4d8b757
enzolda <enzolealdearagao@gmail.com>
2024-05-27 09:38:47 -0300

    Chat está coeso com id visual

--- front_end/wannatalk/src/components/Pratica.jsx
+++ front_end/wannatalk/src/components/Pratica.jsx
@@ -11,8 +11,8 @@ export function Pratica({ roteiro }) {
     const { messages, input, handleInputChange, handleSubmit } = useChat()    
 
     return(
-        <div className="flex min-h-screen bg-[#1E56A0] items-center justify-center">
-            <Card className="w-[800px] bg-[#1E56A0] text-white">
+        <div className="flex min-h-screen bg-[#1E56A0] items-center justify-center max-h-[700px]">
+            <Card className="w-[800px] bg-[#1E56A0] text-white m-[100px]">
                 <CardHeader>
                     <CardTitle className="justify-center">Praticar: {roteiro.titulo}</CardTitle>
                     <CardDescription className="text-white"> {roteiro.resumo} </CardDescription>
