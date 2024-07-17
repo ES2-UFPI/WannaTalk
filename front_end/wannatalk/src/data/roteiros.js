0d065768e25230a41afadcedf1560b97ca9a6ca0
Felipe Moura <felipemoura1407@gmail.com>
2024-07-17 00:33:28 -0300

    Mudando tecnologia de Next.js para React por conta de problemas de desenvolvimento

--- front_end/wannatalk/src/data/roteiros.js
+++
@@ -1,8 +0,0 @@
-const roteiros = [
-    {id: 1, titulo: 'No aeroporto', autor: 'Enzo', rank: 'Novato', praticas: 23, satisfacao: 80, resumo: "Resumo 1"},
-    {id: 2, titulo: 'No aeroporto', autor: 'Jordana', rank: 'Master', praticas: 50, satisfacao: 95, resumo: "Resumo 2"},
-    {id: 3, titulo: 'No aeroporto', autor: 'Felipe', rank: 'Experiente', praticas: 27, satisfacao: 60, resumo: "Resumo 3"},
-    {id: 4, titulo: 'No aeroporto', autor: 'Jhonn', rank: 'Novato', praticas: 40, satisfacao: 35, resumo: "Resumo 4"}
-]
-
-export default roteiros;
