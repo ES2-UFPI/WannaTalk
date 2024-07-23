34232007b387767a85f98c8ede2da67a875da33d
jordanabfranca <jordanafranca@ufpi.edu.br>
2024-07-22 23:29:33 -0300

    Botao de Testar o roteiro a partir do personagem escolhido

--- front_end/src/components/PraticarRoteiroComponent.jsx
+++ front_end/src/components/PraticarRoteiroComponent.jsx
@@ -17,6 +17,7 @@ const PraticarRoteiro = () => {
     const [selectedCharacterId, setSelectedCharacterId] = useState(null);
     const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
     const [isDialogModalOpen, setIsDialogModalOpen] = useState(false);
+    const [currentDialogueIndex, setCurrentDialogueIndex] = useState(0);
 
     useEffect(() => {
         const fetchScript = async () => {
@@ -58,6 +59,12 @@ const PraticarRoteiro = () => {
         setIsDialogModalOpen(false);
     };
 
+    const handleNextDialogue = () => { // Proximo dialogo
+        if (currentDialogueIndex < script.dialoguesList.length - 1) {
+            setCurrentDialogueIndex(currentDialogueIndex + 1);
+        }
+    }
+
     return (
         <div className="min-h-screen flex flex-col items-center bg-gray-100 p-6">
             <h1 className="w-full max-w-4xl py-3 mt-8 text-3xl font-bold mb-6 rounded-xl shadow-xl text-center text-white bg-[#00B2FF]">
@@ -78,51 +85,57 @@ const PraticarRoteiro = () => {
                     Ver Roteiro Completo
                 </button>
             </div>
-            <div className="w-full max-w-2xl  justify-center p-8 bg-[#00B2FF] rounded-xl shadow-xl">
-            
-
-            <div className="w-full max-w-2xl py-3 bg-[#03A0E4] shadow-md rounded-xl p-6 mb-6">
-                <div className="block text-white text-sm font-bold mb-2">Escolha seu Personagens</div>
-                <select
-                    value={selectedCharacterId || ''}
-                    onChange={handleCharacterChange}
-                    className="my-3 flex items-stretch  space-x-2 py-2 px-10 rounded-xl text-[#727171] border leading-tight focus:outline-none focus:shadow-outline"
-                >
-                    <option value="">Selecione um personagem</option>
-                    {Object.keys(characterColors).map(characterId => (
-                        <option key={characterId} value={characterId}>
-                            Personagem {characterId}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <div className="w-full max-w-2xl py-3 bg-white shadow-md rounded-xl p-6 mb-6 overflow-y-auto max-h-[400px]">
-                <h2 className="text-2xl font-semibold mb-4">Diálogos</h2>
-                {script.dialoguesList.length === 0 ? (
-                    <p>Não há diálogos disponíveis.</p>
-
-                ) : (
-                <div>
-                    {script.dialoguesList
-                    .filter(dialogue => !selectedCharacterId || dialogue.characterId === selectedCharacterId)
-                    .map((dialogue, index) => (
-                    <div
-                        key={index}
-                        className={`mb-4 p-4 rounded-lg ${characterColors[dialogue.characterId] || 'bg-gray-200'} relative`}
+            <div className="w-full max-w-2xl justify-center p-8 bg-[#00B2FF] rounded-xl shadow-xl">
+                <div className="w-full max-w-2xl py-3 bg-[#03A0E4] shadow-md rounded-xl p-6 mb-6">
+                    <div className="block text-white text-sm font-bold mb-2">Escolha seu Personagens</div>
+                    <select
+                        value={selectedCharacterId || ''}
+                        onChange={handleCharacterChange}
+                        className="my-3 flex items-stretch space-x-2 py-2 px-10 rounded-xl text-[#727171] border leading-tight focus:outline-none focus:shadow-outline"
                     >
-                        <p><strong>Personagem {dialogue.characterId}:</strong></p>
-                        <p className="pt-4">{dialogue.dialogue}</p>
-                        <div className={` top-0 left-0 w-0 h-0 border-l-8 border-r-8 ${characterColors[dialogue.characterId] || 'border-gray-200'} border-t-8 border-transparent`} style={{ transform: 'translateY(-100%)' }}></div>
-                    </div>
-                ))}
-                </div>
-                   )}
+                        <option value="">Selecione um personagem</option>
+                        {Object.keys(characterColors).map(characterId => (
+                            <option key={characterId} value={characterId}>
+                                Personagem {characterId}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
+                <div className="w-full max-w-2xl py-3 bg-white shadow-md rounded-xl p-6 mb-6 overflow-y-auto max-h-[400px]">
+                    <h2 className="text-2xl font-semibold mb-4">Diálogos</h2>
+                    {script.dialoguesList.length === 0 ? (
+                        <p>Não há diálogos disponíveis.</p>
+                    ) : (
+                        <div>
+                            {script.dialoguesList.slice(0, currentDialogueIndex + 1).map((dialogue, index) => (
+                                <div
+                                    key={index}
+                                    className={`mb-4 p-4 rounded-lg ${characterColors[dialogue.characterId] || 'bg-gray-200'} relative`}
+                                >
+                                    <p><strong>Personagem {dialogue.characterId}:</strong></p>
+                                    <p className="pt-4">{dialogue.dialogue}</p>
+                                    {selectedCharacterId === dialogue.characterId && (
+                                        <button
+                                            onClick={() => console.log(`Testando diálogo ${dialogue.dialogue}`)}
+                                            className="bg-green-500 text-white py-1 px-2 rounded-lg mt-2"
+                                        >
+                                            Testar
+                                        </button>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <button 
+                    onClick={handleNextDialogue}
+                    disabled={currentDialogueIndex >= script.dialoguesList.length - 1}
+                    className={`bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 ${currentDialogueIndex >= script.dialoguesList.length - 1 ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+                >
+                    Continuar
+                </button>
             </div>
-            
-            
 
             {/* Informações Modal */}
             {isInfoModalOpen && (
@@ -131,9 +144,16 @@ const PraticarRoteiro = () => {
                     onClick={closeInfoModal}
                 >
                     <div
-                        className="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl"
+                        className="bg-white p-6 shadow-lg w-full max-w-2xl rounded-xl relative"
                         onClick={(e) => e.stopPropagation()} // Impede o clique dentro do modal de fechar o modal
                     >
+                        <span
+                            onClick={closeInfoModal}
+                            className="absolute top-2 right-2 text-[#727171] cursor-pointer text-2xl z-50"
+                        >
+                            &times;
+                        </span>
+
                         <h2 className="text-2xl font-semibold mb-4">{script.title}</h2>
                         <p className="text-lg mb-4"><strong>Resumo:</strong> {script.summary}</p>
                         <p className="text-lg mb-4"><strong>Notas:</strong> {script.notes}</p>
@@ -141,12 +161,6 @@ const PraticarRoteiro = () => {
                         <p className="text-lg mb-4"><strong>Dificuldade ID:</strong> {script.difficultyId}</p>
                         <p className="text-lg mb-4"><strong>Gênero ID:</strong> {script.genderId}</p>
                         <p className="text-lg mb-4"><strong>Idioma ID:</strong> {script.languageId}</p>
-                        <button
-                            onClick={closeInfoModal}
-                            className="bg-red-500 text-white py-2 px-4 rounded-lg mt-4"
-                        >
-                            Fechar
-                        </button>
                     </div>
                 </div>
             )}
@@ -158,33 +172,33 @@ const PraticarRoteiro = () => {
                     onClick={closeDialogModal}
                 >
                     <div
-                        className="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl"
+                        className="bg-white p-6 rounded-xl shadow-lg w-full max-w-2xl relative"
                         onClick={(e) => e.stopPropagation()} // Impede o clique dentro do modal de fechar o modal
                     >
+                        {/* Botão de fechar no canto superior direito */}
+                        <span
+                            onClick={closeDialogModal}
+                            className="absolute top-2 right-2 text-[#727171] cursor-pointer text-2xl z-50"
+                        >
+                            &times;
+                        </span>
+
                         <h2 className="text-2xl font-semibold mb-4">Diálogos</h2>
                         {script.dialoguesList.length === 0 ? (
                             <p>Não há diálogos disponíveis.</p>
                         ) : (
                             <div>
-                                {script.dialoguesList
-                                    .filter(dialogue => !selectedCharacterId || dialogue.characterId === selectedCharacterId)
-                                    .map((dialogue, index) => (
-                                        <div
-                                            key={index}
-                                            className={`mb-4 p-4 rounded-lg ${characterColors[dialogue.characterId] || 'bg-gray-200'}`}
-                                        >
-                                            <p><strong>Personagem {dialogue.characterId}:</strong></p>
-                                            <p>{dialogue.dialogue}</p>
-                                        </div>
-                                    ))}
+                                {script.dialoguesList.map((dialogue, index) => (
+                                    <div
+                                        key={index}
+                                        className={`mb-4 p-4 rounded-lg ${characterColors[dialogue.characterId] || 'bg-gray-200'}`}
+                                    >
+                                        <p><strong>Personagem {dialogue.characterId}:</strong></p>
+                                        <p>{dialogue.dialogue}</p>
+                                    </div>
+                                ))}
                             </div>
                         )}
-                        <button
-                            onClick={closeDialogModal}
-                            className="bg-red-500 text-white py-2 px-4 rounded-lg mt-4"
-                        >
-                            Fechar
-                        </button>
                     </div>
                 </div>
             )}
