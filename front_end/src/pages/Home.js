6f175b30a8692d0a38c5b01c80851a38a461dec2
Jhonnatas Ribeiro <jhonnatasribeiro01@gmail.com>
2024-08-07 20:16:59 -0300

    adicionei responsividade a home

--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -5,7 +5,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useState } from "react";
-import '../styles/style.home.css'
+import '../styles/style.home.css';
+
 const carouselItems = [
   {
     id: 1,
@@ -53,28 +54,63 @@ export default function Home() {
     arrows: false, // Removendo as setas para simplificar o layout
   };
 
+  // Função para rolar suavemente para a seção específica
+  const scrollToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <main className="bg-gray-50 min-h-screen flex flex-col">
         <div className="flex-1 px-4 py-12">
           {/* Seção Principal com Imagem e Botão */}
           <section className="flex w-full max-w-4xl mx-auto mb-12 principal">
-           <div className="principal-1">
-           <h2 className="text-3xl font-extrabold text-gray-800 mb-6 text-center ">Treinar novas línguas em diversas situações!</h2>
-           <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition duration-300 mb-6">Saber Mais</button>
-           </div>
+            <div className="principal-1">
+              <h2 className="text-3xl font-extrabold text-gray-800 mb-6 text-center">
+                Treinar <mark className="text-blue-500">novas línguas</mark> em diversas situações!
+              </h2>
+              <button
+                onClick={() => scrollToSection('section2')}
+                className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition duration-300 mb-6">
+                Saber Mais
+              </button>
+            </div>
             <div className="flex flex-col items-center">
               <img
                 src="https://www.creativefabrica.com/wp-content/uploads/2021/10/02/Illustration-with-young-people-talking-Graphics-18175733-1.png"
                 className="w-full object-cover rounded-lg"
-                 alt="Imagem principal mostrando um exemplo de treinamento em línguas"
+                alt="Imagem principal mostrando um exemplo de treinamento em línguas"
               />
             </div>
           </section>
-
+          <div className="sessao-bandeiras">
+            <section className="bandeiras">
+              <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/255px-Flag_of_the_United_Kingdom_%283-5%29.svg.png" alt="Inglês" />
+                <p>Inglês</p>
+              </div>
+              <div>
+                <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/2022/01/bandeira-da-espanha.jpg" alt="Espanhol" />
+                <p>Espanhol</p>
+              </div>
+              <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%29.svg/1200px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931974%29.svg.png" alt="Francês" />
+                <p>Francês</p>
+              </div>
+              <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/255px-Flag_of_Germany.svg.png" alt="Alemão" />
+                <p>Alemão</p>
+              </div>
+              <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg" alt="Italiano" />
+                <p>Italiano</p>
+              </div>
+            </section>
+          </div>
           {/* Carrossel */}
           <section className="w-full max-w-4xl mx-auto mb-12">
+            <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Saber Mais</h2>
             <Slider {...settings}>
               {carouselItems.map(item => (
                 <div key={item.id} className="px-4">
@@ -95,7 +131,7 @@ export default function Home() {
           </section>
 
           {/* Bloco Informativo */}
-          <section className="w-full max-w-4xl mx-auto mb-12">
+          <section id="section2" className="w-full max-w-4xl mx-auto mb-12">
             <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
               <div className="bg-blue-600 text-white rounded-lg shadow-lg p-6">
                 <h2 className="text-2xl font-semibold">O que é o WannaTalk?</h2>
@@ -113,10 +149,12 @@ export default function Home() {
           </section>
 
           {/* Seção de Roteiros Populares */}
-          <section className="w-full max-w-4xl mx-auto mb-12">
-            <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Roteiros mais populares do dia</h2>
-            <ListaRoteiros />
-          </section>
+          <div className="roteiros-populares">
+            <section className="w-full max-w-4xl mx-auto mb-12">
+              <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">Roteiros mais populares do dia</h2>
+              <ListaRoteiros />
+            </section>
+          </div>
         </div>
 
         <Rodape />
