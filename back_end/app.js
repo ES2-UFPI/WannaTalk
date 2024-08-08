b2bc31e0d9be94f58e02008efd60f2c2cbc886c3
Felipe Moura <felipemoura.developer@gmail.com>
2024-08-08 09:59:51 -0300

    Integrando branches

--- back_end/app.js
+++ back_end/app.js
@@ -1,58 +1,52 @@
-const express = require('express')
-const app     = express()
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
 
-const prisma = require("./prisma/client")
+const prisma = require("./prisma/client");
 
-const user_router = require("./routes/user")
-const admin_router = require("./routes/admin")
-const api_router = require("./routes/api")
-
-
+const user_router = require("./routes/user");
+const admin_router = require("./routes/admin");
+const api_router = require("./routes/api");
 
 // Configurações
 
-    // Template Engine
-    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
+// Template Engine
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    headers: ["Content-Type"],
+  })
+);
 
-    // Body Parser
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+app.use("/user", user_router);
+app.use("/admin", admin_router);
+app.use("/api", api_router);
 
-
-    // Middleware
-    app.use(cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        headers: ['Content-Type']
-      }));
-
-    app.use("/user", user_router);
-    app.use("/admin", admin_router);
-    app.use("/api", api_router);
-
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    });
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 // Rotas
 
-    app.get("/", (req,res) => {
-        res.json({nome: "teste"})
-    })
-
-
+app.get("/", (req, res) => {
+  res.json({ nome: "teste" });
+});
 
 // Conexão
 
-    const PORT = 5000
-    app.listen(PORT, () => {
-        console.log("Conexão ativa na porta: "+ PORT)
-    })
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log("Conexão ativa na porta: " + PORT);
+});
--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -1,200 +1,194 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const prisma = require('../prisma/client');
+const prisma = require("../prisma/client");
 
 // Middleware de tratamento de erros
-const asyncHandler = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-
 // Rota para renderizar a configuração do chat
-router.get("/:hashcode/chatconfig", asyncHandler(async (req, res) => {
+router.get(
+  "/:hashcode/chatconfig",
+  asyncHandler(async (req, res) => {
     const { hashcode } = req.params;
 
     const user = await prisma.user.findFirst({ where: { hashcode } });
 
     if (!user) {
-        console.log(`404\nError: User with hashcode ${hashcode} not found`);
-        return res.redirect("/admin/userlist");
+      console.log(`404\nError: User with hashcode ${hashcode} not found`);
+      return res.redirect("/admin/userlist");
     }
 
     const [context_list, language_list, agent_list] = await Promise.all([
-        prisma.context.findMany(),
-        prisma.language.findMany(),
-        prisma.agent.findMany()
+      prisma.context.findMany(),
+      prisma.language.findMany(),
+      prisma.agent.findMany(),
     ]);
 
     res.render("user/chatconfig", {
-        agents: agent_list,
-        languages: language_list,
-        contexts: context_list
+      agents: agent_list,
+      languages: language_list,
+      contexts: context_list,
     });
-}));
-
-
+  })
+);
 
 // Rota para acessar o chat
-router.get("/:hashcode/chat", asyncHandler(async (req, res) => {
+router.get(
+  "/:hashcode/chat",
+  asyncHandler(async (req, res) => {
     const { hashcode } = req.params;
 
     const user = await prisma.user.findFirst({ where: { hashcode } });
 
     if (!user) {
-        console.log(`404\nError: User with hashcode ${hashcode} not found`);
-        return res.redirect("/admin/userlist");
+      console.log(`404\nError: User with hashcode ${hashcode} not found`);
+      return res.redirect("/admin/userlist");
     }
 
     res.json(user);
     // res.render("/target") // Renderizar a tela do chat
-}));
+  })
+);
 
+// Rota para pesquisar
+const cache = new Map();
+const CACHE_LIMIT = 100;
+const CACHE_TTL = 10 * 60 * 1000;
 
+router.get("/search/:query", async (req, res) => {
+  const query = req.params.query;
+  const limit = 10;
+  const page = 1;
+  const skip = (page - 1) * limit;
 
+  if (!query) {
+    return res.status(400).send({ error: "Query parameter is required" });
+  }
 
+  const cacheKey = `${query}-${page}-${limit}`;
+  const now = Date.now();
 
-// Rota para pesquisar 
-router.get('/:hashcode/search/:query', async (req, res) => {
-    const query  = req.params.query;
-    limit = 10, page = 1 
-    // const { query, limit = 10, page = 1 } = req.query; // conversar com o front
-    
-    if (!query) {
-        return res.status(400).send({ error: 'Query parameter is required' });
+  // Verifica se o resultado está em cache e se ainda é válido
+  if (cache.has(cacheKey) && now - cache.get(cacheKey).timestamp < CACHE_TTL) {
+    return res.json(cache.get(cacheKey).data);
+  }
+
+  try {
+    const roteiros = await prisma.script.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: "insensitive",
+        },
+      },
+      select: {
+        title: true,
+        description: true,
+      },
+      take: parseInt(limit),
+      skip: parseInt(skip),
+    });
+
+    const responseData = { roteiros };
+
+    cache.set(cacheKey, { timestamp: now, data: responseData });
+
+    if (cache.size > CACHE_LIMIT) {
+      const oldestKey = cache.keys().next().value;
+      cache.delete(oldestKey);
     }
-    
-    const skip = (page - 1) * limit; // paginação, futuramente puxar do front a informação
 
-    try {
-        const [users, roteiros, contextos, idiomas] = await Promise.all([
-            prisma.user.findMany({
-                where: {
-                    username: {
-                        contains: query,
-                        mode: 'insensitive',
-                    },
-                },
-                select: {
-                    name: true,
-                    username: true,
-                },
-                take: parseInt(limit),
-                skip: parseInt(skip),
-            }),
-            prisma.script.findMany({
-                where: {
-                    title: {
-                        contains: query,
-                        mode: 'insensitive',
-                    },
-                },
-                select: {
-                    title: true,
-                    description: true,
-                },
-                take: parseInt(limit),
-                skip: parseInt(skip),
-            }),
-            prisma.context.findMany({
-                where: {
-                    name: {
-                        contains: query,
-                        mode: 'insensitive',
-                    },
-                },
-                select: {
-                    name: true,
-                    code: true,
-                },
-                take: parseInt(limit),
-                skip: parseInt(skip),
-            }),
-            prisma.language.findMany({
-                where: {
-                    name: {
-                        contains: query,
-                        mode: 'insensitive',
-                    },
-                },
-                select: {
-                    name: true,
-                    code: true,
-                },
-                take: parseInt(limit),
-                skip: parseInt(skip),
-            })
-        ])
-        
-        res.json({
-                users,
-                roteiros,
-                contextos,
-                idiomas,
-            });
-        
-    
-        
-    } catch (err) {
-        console.error('Error fetching data:', err);
-        res.status(500).send({ error: 'An error occurred while fetching data' });
-    }
+    res.json(responseData);
+  } catch (err) {
+    console.error("Error fetching scripts:", err);
+    res.status(500).send({ error: "An error occurred while fetching scripts" });
+  }
 });
 
-router.post('/criarRoteiro', asyncHandler(async (req, res) => {
-    const {
-        resumo,
-        dificuldade,
-        idiomas,
-        genero,
-        notas,
-        referencias,
-        title
-    } = req.body;
+class Roteiro {
+  constructor(
+    title,
+    summary,
+    difficultyId,
+    generoId,
+    idiomasId,
+    notes = "",
+    refs = ""
+  ) {
+    this.title = title;
+    this.summary = summary;
+    this.difficultyId = difficultyId;
+    this.generoId = generoId;
+    this.idiomasId = idiomasId;
+    this.notes = notes;
+    this.refs = refs;
+  }
+
+  clone() {
+    return new Roteiro(
+      this.title,
+      this.summary,
+      this.difficultyId,
+      this.generoId,
+      this.idiomasId,
+      this.notes,
+      this.refs
+    );
+  }
+}
+
+router.post(
+  "/criarRoteiro",
+  asyncHandler(async (req, res) => {
+    const { resumo, dificuldade, idiomas, genero, notas, referencias, title } =
+      req.body;
 
     if (!title || !resumo || !dificuldade || !genero || !idiomas) {
-        return res.status(400).send({ error: 'Campos obrigatórios estão faltando' });
+      return res
+        .status(400)
+        .send({ error: "Campos obrigatórios estão faltando" });
     }
 
-    // Converta os IDs para inteiros
     const dificuldadeId = parseInt(dificuldade, 10);
     const generoId = parseInt(genero, 10);
     const idiomasId = parseInt(idiomas, 10);
 
     if (isNaN(dificuldadeId) || isNaN(generoId) || isNaN(idiomasId)) {
-        return res.status(400).send({ error: 'IDs inválidos' });
+      return res.status(400).send({ error: "IDs inválidos" });
     }
 
-    summary = resumo;
-    refs = referencias;
-    notes = notas;
+    const prototipoRoteiro = new Roteiro(
+      title,
+      resumo,
+      dificuldadeId,
+      generoId,
+      idiomasId,
+      notas,
+      referencias
+    );
 
     try {
-        const novoRoteiro = await prisma.script.create({
-            data: {
-                title,
-                summary,
-                difficulty: { connect: { id: dificuldadeId } },
-                gender: { connect: { id: generoId } },
-                language: { connect: { id: idiomasId } },
-                notes,
-                refs
-            }
-        });
+      const novoRoteiro = await prisma.script.create({
+        data: {
+          title: prototipoRoteiro.title,
+          summary: prototipoRoteiro.summary,
+          difficulty: { connect: { id: prototipoRoteiro.difficultyId } },
+          gender: { connect: { id: prototipoRoteiro.generoId } },
+          language: { connect: { id: prototipoRoteiro.idiomasId } },
+          notes: prototipoRoteiro.notes,
+          refs: prototipoRoteiro.refs,
+        },
+      });
 
-        console.log("ROTEIRO CRIADO");
-        res.status(201).json(novoRoteiro);
+      console.log("ROTEIRO CRIADO");
+      res.status(201).json(novoRoteiro);
     } catch (err) {
-        console.error('Erro ao criar roteiro:', err);
-        res.status(500).send({ error: 'Erro ao criar roteiro' });
+      console.error("Erro ao criar roteiro:", err);
+      res.status(500).send({ error: "Erro ao criar roteiro" });
     }
-}));
-
-
-
-
-
-
-
-
+  })
+);
 
 module.exports = router;
