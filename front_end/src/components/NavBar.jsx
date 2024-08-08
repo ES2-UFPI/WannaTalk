8b792af9e1b3685ef478ff6a96b850e139df26f9
Felipe Moura <felipemoura.developer@gmail.com>
2024-08-08 04:08:00 -0300

    adicionando vozes ao banco

--- front_end/src/components/NavBar.jsx
+++ front_end/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import '../styles/style.navbar.css';
-import { Link } from 'react-router-dom';
-import Pesquisar from './Pesquisar';
+import React from "react";
+import "../styles/style.navbar.css";
+import { Link } from "react-router-dom";
+import Pesquisar from "./Pesquisar";
 function NavBar() {
   return (
     <nav className="navbar flex flex-row justify-between">
@@ -9,16 +9,17 @@ function NavBar() {
         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div className="flex flex-shrink-0 items-center">
             {/* Vai direcionar para tela inicial */}
-            <Link to='/' className="linkHome">WannaTalk?</Link>
+            <Link to="/" className="linkHome">
+              WannaTalk?
+            </Link>
           </div>
           <div className="hidden sm:ml-6 sm:block">
             <div className="flex space-x-1">
               {/* <!-- Direciona para tela de visualizar roteiros/praticar roteiro--> */}
-              <Link to="/praticarRoteiro" className="linkPratica">Praticar Roteiro</Link>
-              <Link to="/criarRoteiro" className="linkCriarRoteiro">Criar Roteiro</Link>
-              <Link to="/search" className="linkPesquisar">Pesquisar</Link>
+              <Link to="/criarRoteiro" className="linkCriarRoteiro">
+                Criar Roteiro
+              </Link>
               <Link to="/configurarChat">Chat</Link>
-
             </div>
           </div>
         </div>
@@ -26,4 +27,5 @@ function NavBar() {
       <Pesquisar />
     </nav>
   );
-} export default NavBar;
+}
+export default NavBar;
--- front_end/src/components/Personas.jsx
+++ front_end/src/components/Personas.jsx
@@ -1,79 +1,132 @@
-import React, {useState} from 'react';
-import '../styles/style.persona.css';
+import React, { useState, useEffect } from "react";
+import "../styles/style.persona.css";
 
 const Persona = () => {
-    const voiceColors = {
-        'Voz 1': 'bg-[#FF3390]',
-        'Voz 2': 'bg-[#00B2FF]',
-        'Voz 3': 'bg-[#FFB800]'
-    };
+  const voiceColors = {
+    "Voz 1": "bg-[#FF3390]",
+    "Voz 2": "bg-[#00B2FF]",
+    "Voz 3": "bg-[#FFB800]",
+  };
 
-    const [availableVoices, setAvailableVoices] = useState(['Voz 1', 'Voz 2', 'Voz 3']);
-    const [selectedVoice, setSelectedVoice] = useState('');
-    const [selectedName, setSelectedName] = useState('');
-    const [confirmedVoices, setConfirmedVoices] = useState([]);
+  const [availableVoices, setAvailableVoices] = useState([]);
+  const [selectedVoice, setSelectedVoice] = useState("");
+  const [selectedName, setSelectedName] = useState("");
+  const [confirmedVoices, setConfirmedVoices] = useState([]);
 
-    const handleSelectChange = (event) => {
-        setSelectedVoice(event.target.value);
-        
-    };
+  useEffect(() => {
+    fetch("http://localhost:5000/api/getVoices")
+      .then((response) => response.json())
+      .then((data) => setAvailableVoices(data.map((item) => item.voice)))
+      .catch((error) => console.error("Erro ao buscar vozes:", error));
+  }, []);
 
-    const handleNameChange = (event) => {
-        
-        const value = event.target.value;
-        if (value.length <= 12) {
-            setSelectedName(value);
-        }
-    };
+  const handleSelectChange = (event) => {
+    setSelectedVoice(event.target.value);
+  };
 
-    const handleConfirm = () => {
-        setAvailableVoices(availableVoices.filter(voice => voice !== selectedVoice));
-        setConfirmedVoices([...confirmedVoices, { voice: selectedVoice, name: selectedName }]);
-        setSelectedVoice('');
-        setSelectedName('');
-    };
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    if (value.length <= 12) {
+      setSelectedName(value);
+    }
+  };
 
-    const handleRevert = (voice) => {
-        const voiceToRevert = confirmedVoices.find(v => v.voice === voice);
-        if (voiceToRevert) {
-            setConfirmedVoices(confirmedVoices.filter(v => v.voice !== voice));
-            setAvailableVoices([...availableVoices, voice]);
-        }
-    };
+  const handleConfirm = () => {
+    setAvailableVoices(
+      availableVoices.filter((voice) => voice !== selectedVoice)
+    );
+    setConfirmedVoices([
+      ...confirmedVoices,
+      { voice: selectedVoice, name: selectedName },
+    ]);
+    setSelectedVoice("");
+    setSelectedName("");
+  };
 
-    return(
-        <div className='persona w-full py-2 px-3  rounded-2xl'>
-            <div className="block text-white text-sm font-bold mb-2">Personagens</div>
-            <div className='personagens my-5 flex items-stretch  space-x-2'>   
-                <input id='nome' type='text' value={selectedName} onChange={handleNameChange} placeholder='Nome do Personagem' maxLength={12} className="shadow appearance-none border rounded-xl py-2 px-3 text-[#727171] leading-tight focus:outline-none focus:shadow-outline flex-grow" ></input>
-                {  (<select id='voz' value={selectedVoice} onChange={handleSelectChange} className='shadow aparence-none text-[#727171] rounded-xl p-2 border leading-tight focus:outline-none focus:shadow-outline'>
-                    <option value="" disabled>Escolha uma voz</option>
-                    {availableVoices.map((voice, index) => (
-                        <option key={index} value={voice}>{voice}</option>
-                    ))}
-                </select>)}
-                <button type='button' onClick={handleConfirm} disabled={!selectedVoice || !selectedName} className='bg-[#8CEAFF] text-[#727171] rounded-xl min-h-[10px] p-2 flex-grow'>Adicionar</button>
-            </div>
+  const handleRevert = (voice) => {
+    const voiceToRevert = confirmedVoices.find((v) => v.voice === voice);
+    if (voiceToRevert) {
+      setConfirmedVoices(confirmedVoices.filter((v) => v.voice !== voice));
+      setAvailableVoices([...availableVoices, voice]);
+    }
+  };
 
-            <div className='balao'>
-                {confirmedVoices.length > 0 && (
-                    <div className='bg-white p-2 rounded-lg flex flex-row'>
-                        {confirmedVoices.map((item, index) => (
-                            <div key={index} className='flex flex-row items-center justify-between mr-2 mb-2'>
-                                <button onClick={() => handleRevert(item.voice)} className={`${voiceColors[item.voice]} flex flex-row min-h-[10px] py-1 px-2 pl-2 items-center rounded flex-grow`}>
-                                    {item.name} ({item.voice})
-                                    <svg className="ml-2 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293
-                                        2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
-                                    </svg>
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+  return (
+    <div className="persona w-full py-2 px-3 rounded-2xl">
+      <div className="block text-white text-sm font-bold mb-2">Personagens</div>
+      <div className="personagens my-5 flex items-stretch space-x-2">
+        <input
+          id="nome"
+          type="text"
+          value={selectedName}
+          onChange={handleNameChange}
+          placeholder="Nome do Personagem"
+          maxLength={12}
+          className="shadow appearance-none border rounded-xl py-2 px-3 text-[#727171] leading-tight focus:outline-none focus:shadow-outline flex-grow"
+        />
+        <select
+          id="voz"
+          value={selectedVoice}
+          onChange={handleSelectChange}
+          className="shadow aparence-none text-[#727171] rounded-xl p-2 border leading-tight focus:outline-none focus:shadow-outline"
+        >
+          <option value="" disabled>
+            Escolha uma voz
+          </option>
+          {availableVoices.map((voice, index) => (
+            <option key={index} value={voice}>
+              {voice}
+            </option>
+          ))}
+        </select>
+        <button
+          type="button"
+          onClick={handleConfirm}
+          disabled={!selectedVoice || !selectedName}
+          className="bg-[#8CEAFF] text-[#727171] rounded-xl min-h-[10px] p-2 flex-grow"
+        >
+          Adicionar
+        </button>
+      </div>
+
+      <div className="balao">
+        {confirmedVoices.length > 0 && (
+          <div className="bg-white p-2 rounded-lg flex flex-row">
+            {confirmedVoices.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-row items-center justify-between mr-2 mb-2"
+              >
+                <button
+                  onClick={() => handleRevert(item.voice)}
+                  className={`${
+                    voiceColors[item.voice]
+                  } flex flex-row min-h-[10px] py-1 px-2 pl-2 items-center rounded flex-grow`}
+                >
+                  {item.name} ({item.voice})
+                  <svg
+                    className="ml-2 w-6 h-6 text-gray-800 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Persona;
